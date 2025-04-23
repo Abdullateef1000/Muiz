4846f075982f3dab6a2d2a7606d8cd0c154f6f18
Abdullateef1000 <122871274+Abdullateef1000@users.noreply.github.com>
2025-04-23 11:54:20 +0100

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("how are you?")
+
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("how are you doing pretty?")
+
+
+
+    End Sub
+End Class
